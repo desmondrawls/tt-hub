4d87901cb07a34f2517961c87bb498a66dcd351a
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2015-12-14 20:25:07 -0500

    Remove superfluous console.logging.

--- public/bundle.js
+++ public/bundle.js
@@ -47637,26 +47637,21 @@ var Double = React.createClass({displayName: "Double",
     fetchResources: function(){
         var context = this
         var links =_.filter(context.props.collectionObject.collection.links, function(link){ return link.rel == 'resource' })
-        console.log("LINKS:", links)
         var hrefs = _.map(links, function(link){ return link.href})
-        console.log("HREFS:", hrefs)
         var resourcePromises = _.map(hrefs, function(href){return context.fetchResource(href)})
         Q.all(resourcePromises).then(function(resources){
-            console.log("RESOURCES", resources)
             context.setState({resources: resources})
             context.setState({loaded: true})
         })
     },
 
     fetchResource: function(href){
-        console.log("LINK TO", href)
         return $.ajax(href, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json'
             },
             success: function(collectionObject){
-                console.log("FETCHED A RESOURCE:", collectionObject)
                 return collectionObject
             }
         })
